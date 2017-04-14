3644dfef697bf0e337486f9909c1a39ca90d1004
zarabon <freestalker1987@gmail.com>
2017-04-14 23:02:05 +0300

    refactoring

--- lab1/src/Task2.java
+++ lab1/src/Task2.java
@@ -8,11 +8,12 @@ import java.util.concurrent.Future;
 /**
  * Первый аргумент - х
  * второй - точность
- * */
+ */
 
 public class Task2 {
 
-    public static void main(String...args) throws Exception {
+    public static void main(String... args) throws Exception {
+        //Читаем args
         ArrayList<String> runArgs = new ArrayList<String>(Arrays.asList(args));
         if (runArgs.size() < 2) {
             throw new Exception("Not enough args to run");
@@ -22,37 +23,40 @@ public class Task2 {
         Double accuracy = Double.valueOf(runArgs.get(1));
 
         //считаем слева
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Future<Double> funcResult = executorService.submit(() -> Math.cosh(x));
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        Future<Double> funcResult = singleThreadExecutor.submit(() -> Math.cosh(x));
 
         //считаем справа до точности
+        double sum = 0;
         boolean flag = true;
         int n = 0;
         Double prev = null;
-        double sum = 0;
+        //Эта сложна функция суммы
         while (flag) {
-
             double compRes = Math.pow(x, 2 * n) / factorial(2 * n);
             n++;
             sum = sum + compRes;
 
-            if (prev!=null && Math.abs(prev - compRes)<accuracy ) {
+            if (prev != null && Math.abs(prev - compRes) < accuracy) {
                 flag = false;
             }
 
             prev = compRes;
         }
 
+        singleThreadExecutor.shutdown();
+
+        //Выводим
         System.out.println(sum);
         System.out.println(funcResult.get());
-        executorService.shutdown();
+
 
     }
 
     /**
-     * @param  number double
+     * @param number double
      * @return factorial
-     * */
+     */
     public static double factorial(double number) {
         if (number <= 1) return 1;
         else return number * factorial(number - 1);
