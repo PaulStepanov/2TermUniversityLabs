677f5b0b043985b7ad30b880ae98c88a7719996f
zarabon <freestalker1987@gmail.com>
2017-04-14 20:38:44 +0300

    University lab 1 sem

--- lab1/src/Task1.java
+++ lab1/src/Task1.java
@@ -1,8 +1,19 @@
-/**
- * Created by pili on 3/4/17.
- */
-public class Task1 {
-    public static void main(String...args){
-        
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Task1 {
+    public static void main(String... args) throws Exception {
+        ArrayList<String> runArgs = new ArrayList<String>(Arrays.asList(args));
+        if (runArgs.size() < 3) {
+            throw new Exception("Not enough args to run");
+        }
+        Collections.reverse(runArgs);
+        System.out.println(
+                runArgs.stream()
+                        .map(Double::parseDouble)
+                        .reduce(0d, (sum, elem) ->
+                                1 / (sum + elem)
+                        ));
     }
 }
